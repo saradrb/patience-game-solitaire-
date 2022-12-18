374472d34c63ab269cdaa4017764f6f905109592
DORBANE sara <sara.dorbane@etu.u-paris.fr>
2022-12-18 21:37:26 +0100

    Update XpatSolver.ml

--- src/XpatSolver.ml
+++ src/XpatSolver.ml
@@ -1,7 +1,24 @@
 
 open XpatLib
+open Card
 
 type game = Freecell | Seahaven | Midnight | Baker
+
+type init_rule = {nb_col:int; nb_depots:int; nb_reg:int}
+type card = Card.card
+
+(* une colonne est une liste de cartes *)
+type colonne = Card.card List.t
+
+(* un depeau est un tableau d'entier ou chaque case represente une couleur et contient le numero de carte dedans *)
+type depot= int PArray.t (* case 0:Trefle ; case 1:Pique ;case 2:Coeur ; case 3:Carreau *)
+
+(* registres est un tableau ou chaque case represente un registre qui contient une carte ou vide*)
+type registres = Card.card option PArray.t
+
+(*l'etat du jeux contient les depots actuels , les registres actuels et la liste de toute les colonnes*)
+type etat = {depots: depot; registres: registres option  ;colonnes: colonne FArray.t}
+
 
 type mode =
   | Check of string (* filename of a solution file to check *)
@@ -30,6 +47,126 @@ let set_game_seed name =
   with _ -> failwith ("Error: <game>.<number> expected, with <game> in "^
                       "FreeCell Seahaven MidnightOil BakersDozen")
 
+
+let get_rule name = function 
+|Freecell-> {nb_col=8;nb_depots=4;nb_reg=4}
+|Seahaven->{nb_col=10;nb_depots=4;nb_reg=4}
+|Midnight->{nb_col=18;nb_depots=4;nb_reg=0}
+|Baker ->{nb_col=13;nb_depots=4;nb_reg=0}
+
+let init_game name = match name with 
+|Freecell-> { 
+    depots=PArray.make 4 0;
+    registres= Some (PArray.make 4 None);
+    colonnes=FArray.make 8 []} 
+|Seahaven->{ 
+    depots=PArray.make 4 0;
+    registres= Some (PArray.make 4 None);
+    colonnes=FArray.make 10 []} 
+|Baker->{ 
+    depots=PArray.make 4 0;
+    registres=None;
+    colonnes=FArray.make 13 []} 
+|Midnight->{ 
+    depots=PArray.make 4 0;
+    registres=None;
+    colonnes=FArray.make 18 []} 
+  
+
+(*fonction qui remplie une colonne avec n cartes*)
+let rec fill_col col cards n=
+  if n>0 then 
+    match (col,cards) with 
+    |([],x::l)-> fill_col [x] l (n-1)
+    |(_,x1::l) -> fill_col (x1::col) l (n-1)
+    |(_,[])-> col
+  else col 
+
+ (* fonction qui remplie n registres par une carte chacun *)
+let rec fill_reg n registres cards= 
+  if n=0 then Some(registres)
+  else match cards with 
+  |[]-> failwith "plus de cartes"
+  |x::l-> match registres with
+          |Some reg ->fill_reg  (n-1) (PArray.set reg (n-1) Some(x)) l 
+          |None -> None
+  
+
+(* distribution des cartes selon les regles de SeaHeaven 
+* 10 colonnes ou chaque une contient 5 carte et deux cartes dans les registres
+*)
+
+let distribution_sv etat permut =
+  let {depots;registres;colonnes}= etat in
+  let rec fill_all_col n colonnes permut =
+    match n with
+    |e when e=0 -> colonnes 
+    |_ -> fill_all_col (n-1) (FArray.set colonnes (n-1) (fill_col [] permut 5)) (List.filteri (fun i a -> i>4) permut)
+  in 
+  {depots = depots; registres = fill_reg 2 registres permut; colonnes = fill_all_col 10 colonnes (List.filteri (fun i a -> i>1) permut)}  
+
+
+(* distribution des cartes selon les regles de Midnight oil
+* *)
+let distribution_md etat permut =
+  let {depots;registres;colonnes}= etat in
+  let rec fill_all_col n colonnes permut =
+    match n with 
+    | e when e=0 -> colonnes 
+    |_ -> fill_all_col (n-1) (FArray.set colonnes (n-1) (fil_col [] permut 3)) (List.filteri (fun i a -> i>2) permut)
+  in 
+{depots = depots; registres = None; colonnes = fill_all_col 18 colonnes permut}  
+
+(* distribution des cartes selon les regles de Baker dozen*)
+let distribution_bk etat permut =
+  let {depots;registres;colonnes}= etat in
+  let rec fill_all_col n colonnes permut =
+    match n with 
+    | e when e=0 -> colonnes 
+    |_ -> fill_all_col (n-1) (FArray.set colonnes (n-1) (fil_col [] permut 5)) (List.filteri (fun i a -> i>2) permut)
+  in 
+{depots = depots; registres = None; colonnes = fill_all_col 18 colonnes permut}  
+
+
+
+
+(* distribution des cartes selon les regles de FreeCell *)
+
+let distribution_fc etat permut=
+  let {depots;registres;colonnes}= etat in
+    let rec fill_all_col num_c colonnes permut =
+      match num_c with
+      |e when e=0 -> colonnes 
+      | _ -> if((num_c mod 2)=0) then 
+        fill_all_col (num_c-1) (FArray.set colonnes (num_c-1) (fill_col  [] permut 7))  (List.filteri (fun i a -> i>6) permut)
+          else fill_all_col (num_c-1) (FArray.set colonnes (num_c-1) (fill_col  [] permut 6))  (List.filteri (fun i a -> i>5) permut)
+    in
+    {depots = depots; registres = registres; colonnes = fill_all_col 8 colonnes permut}
+
+
+(*Affichage des colonnes *)
+let print_col list_card =
+  List.iter (fun card -> Printf.printf "%s " (Card.to_string card)) list_card
+
+let print_colonnes colonnes_tab = 
+  FArray.iter (fun col ->Printf.printf "COL: "; List.iter (fun card -> Printf.printf "%s " (Card.to_string card)) col;Printf.printf "\n" ) colonnes_tab
+
+(*initialistation du jeux et distribution des cartes*)
+let init_distribution etat name permut =  match name with 
+
+  |Freecell-> distribution_fc etat permut
+  |Seahaven-> distribution_sv etat permut 
+  |Midnight-> distribution_Md etat permut 
+  |Baker-> distribution_bk etat permut  
+
+
+
+
+
+
+
+
+
 (* TODO : La fonction suivante est à adapter et continuer *)
 
 let treat_game conf =
@@ -39,9 +176,15 @@ let treat_game conf =
   print_newline ();
   List.iter (fun n -> Printf.printf "%s " (Card.to_string (Card.of_num n)))
     permut;
+
   print_newline ();
-  print_string "C'est tout pour l'instant. TODO: continuer...\n";
-  exit 0
+  let new_etat = init_distribution init_game conf.game permut;
+
+  (*print_string "C'est tout pour l'instant. TODO: continuer...\n";
+  let cards=List.map (fun a->Card.of_num a) permut in 
+  print_colonnes (distribution_fc (init_game config.game ) cards);
+   *)
+  exit 0;
 
 let main () =
   Arg.parse
