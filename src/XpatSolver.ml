a11ccb2a77852e63b5fa68f2762146dc1b56c3ec
dorbanes <sara.dorbane@etu.u-paris.fr>
2022-12-20 17:02:57 +0100

    update solver still syntax error

--- src/XpatSolver.ml
+++ src/XpatSolver.ml
@@ -1,3 +1,4 @@
+
 open XpatLib
 open Card
 
@@ -9,18 +10,22 @@ type card = Card.card
 (* une colonne est une liste de cartes *)
 type colonne = Card.card List.t
 
-(* les 4 depots est un tableau d'entier ou chaque case represente une couleur et contient le numero de carte dedans *)
+(* un depeau est un tableau d'entier ou chaque case represente une couleur et contient le numero de carte dedans *)
 type depot= int PArray.t (* case 0:Trefle ; case 1:Pique ;case 2:Coeur ; case 3:Carreau *)
 
-(* Les registres sont representé par un tableau ou chaque case represente un registre qui contient une carte ou vide*)
-type registres = Card.card option PArray.t
+(* registres est un tableau ou chaque case represente un registre qui contient une carte ou vide*)
+
+type registres = Card.card PArray.t
+
 
 (*l'etat du jeux contient les depots actuels , les registres actuels et la liste de toute les colonnes*)
 type etat = {depots: depot; registres: registres option  ;colonnes: colonne FArray.t}
 
+
 type mode =
   | Check of string (* filename of a solution file to check *)
   | Search of string (* filename where to write the solution *)
+
 
 type config = { mutable game : game; mutable seed: int; mutable mode: mode }
 let config = { game = Freecell; seed = 1; mode = Search "" }
@@ -47,28 +52,29 @@ let set_game_seed name =
 
 
 let get_rule name = function 
-  |Freecell-> {nb_col=8;nb_depots=4;nb_reg=4}
-  |Seahaven-> {nb_col=10;nb_depots=4;nb_reg=4}
-  |Midnight-> {nb_col=18;nb_depots=4;nb_reg=0}
-  |Baker -> {nb_col=13;nb_depots=4;nb_reg=0}
+|Freecell-> {nb_col=8;nb_depots=4;nb_reg=4}
+|Seahaven->{nb_col=10;nb_depots=4;nb_reg=4}
+|Midnight->{nb_col=18;nb_depots=4;nb_reg=0}
+|Baker ->{nb_col=13;nb_depots=4;nb_reg=0}
 
 let init_game name = match name with 
-  |Freecell-> { 
-      depots=PArray.make 4 0;
-      registres= Some (PArray.make 4 None);
-      colonnes=FArray.make 8 []} 
-  |Seahaven->{ 
-      depots=PArray.make 4 0;
-      registres= Some (PArray.make 4 None);
-      colonnes=FArray.make 10 []} 
-  |Baker->{ 
-      depots=PArray.make 4 0;
-      registres=None;
-      colonnes=FArray.make 13 []} 
-  |Midnight->{ 
-      depots=PArray.make 4 0;
-      registres=None;
-      colonnes=FArray.make 18 []} 
+|Freecell-> { 
+    depots=PArray.make 4 0;
+    registres= Some (PArray.make 4 (-1,Trefle));
+    colonnes=FArray.make 8 []} 
+|Seahaven->{ 
+    depots=PArray.make 4 0;
+    registres= Some (PArray.make 4  (-1,Trefle));
+    colonnes=FArray.make 8 []} 
+
+|Baker->{ 
+    depots=PArray.make 4 0;
+    registres=None;
+    colonnes=FArray.make 13 []} 
+|Midnight->{ 
+    depots=PArray.make 4 0;
+    registres=None;
+    colonnes=FArray.make 18 []} 
   
 
 (*fonction qui remplie une colonne avec n cartes*)
@@ -80,13 +86,13 @@ let rec fill_col col cards n=
     |(_,[])-> col
   else col 
 
-(* fonction qui remplie n registres par une carte chacun *)
+ (* fonction qui remplie n registres par une carte chacun *)
 let rec fill_reg n registres cards= 
-  if n=0 then Some(registres)
+  if n=0 then registres
   else match cards with 
   |[]-> failwith "plus de cartes"
-  |x::l-> match registres with
-          |Some reg ->fill_reg  (n-1) (PArray.set reg (n-1) Some(x)) l 
+  |x::l->  match registres with
+          |Some(reg) ->fill_reg  (n-1) (Some(PArray.set reg (n-1) x))  l  
           |None -> None
   
 
@@ -111,7 +117,8 @@ let distribution_md etat permut =
   let rec fill_all_col n colonnes permut =
     match n with 
     | e when e=0 -> colonnes 
-    |_ -> fill_all_col (n-1) (FArray.set colonnes (n-1) (fil_col [] permut 3)) (List.filteri (fun i a -> i>2) permut)
+    |_ -> fill_all_col (n-1) (FArray.set colonnes (n-1) (fill_col [] permut 3)) (List.filteri (fun i a -> i>2) permut)
+
   in 
 {depots = depots; registres = None; colonnes = fill_all_col 18 colonnes permut}  
 
@@ -121,12 +128,16 @@ let distribution_bk etat permut =
   let rec fill_all_col n colonnes permut =
     match n with 
     | e when e=0 -> colonnes 
-    |_ -> fill_all_col (n-1) (FArray.set colonnes (n-1) (fil_col [] permut 5)) (List.filteri (fun i a -> i>2) permut)
+    |_ -> fill_all_col (n-1) (FArray.set colonnes (n-1) (fill_col [] permut 5)) (List.filteri (fun i a -> i>2) permut)
+
   in 
 {depots = depots; registres = None; colonnes = fill_all_col 18 colonnes permut}  
 
 
+
+
 (* distribution des cartes selon les regles de FreeCell *)
+
 let distribution_fc etat permut=
   let {depots;registres;colonnes}= etat in
     let rec fill_all_col num_c colonnes permut =
@@ -148,11 +159,15 @@ let print_colonnes colonnes_tab =
 
 (*initialistation du jeux et distribution des cartes*)
 let init_distribution etat name permut =  
+  let cards = List.map (fun a -> Card.of_num a) permut in 
   match name with 
-  |Freecell-> distribution_fc etat permut
-  |Seahaven-> distribution_sv etat permut 
-  |Midnight-> distribution_Md etat permut 
-  |Baker-> distribution_bk etat permut  
+  |Freecell-> distribution_fc etat cards
+  |Seahaven-> distribution_sv etat cards 
+  |Midnight-> distribution_md etat cards 
+  |Baker-> distribution_bk etat cards  
+
+
+
 
 
 
@@ -171,27 +186,31 @@ let treat_game conf =
     permut;
 
   print_newline ();
-  let new_etat = init_distribution init_game conf.game permut;
+<<<<<<< HEAD
+  init_distribution (init_game conf.game) conf.game permut;
+  (* print_colonnes etat.colonnes; *)
+
+
+  (* print_string "C'est tout pour l'instant. TODO: continuer...\n";
+  let cards=List.map (fun a->Card.of_num a) permut in 
+  print_colonnes (distribution_fc (init_game config.game ) cards);
+   *)
+ 
+
 
   (*print_string "C'est tout pour l'instant. TODO: continuer...\n";
   let cards=List.map (fun a->Card.of_num a) permut in 
   print_colonnes (distribution_fc (init_game config.game ) cards);
    *)
-  exit 0;
-
-
 
 let main () =
-  Arg.parse
-    [("-check", String (fun filename -> config.mode <- Check filename),"<filename>:\tValidate a solution file");
-     ("-search", String (fun filename -> config.mode <- Search filename),
-        "<filename>:\tSearch a solution and write it to a solution file")]
-    set_game_seed (* pour les arguments seuls, sans option devant *)
-    "XpatSolver <game>.<number> : search solution for Xpat2 game <number>";
-  treat_game config
-;;
-
-let _ = if not !Sys.interactive then main () else ();;
+  [("-check", String (fun filename -> config.mode <- Check filename),
+  "<filename>:\tValidate a solution file");
+("-search", String (fun filename -> config.mode <- Search filename),
+  "<filename>:\tSearch a solution and write it to a solution file")]
+set_game_seed (* pour les arguments seuls, sans option devant *)
+"XpatSolver <game>.<number> : search solution for Xpat2 game <number>";
+treat_game config
 
 
-
+let _ = if not !Sys.interactive then main () else ()
